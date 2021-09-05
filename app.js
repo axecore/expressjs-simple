ff5cae6662d6a872323ed03ee72cd165c94d404d
axecore <aldirenaldy323@gmail.com>
2021-09-05 12:45:15 +0000

    add socketjs

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ const
     port = process.env.APP_PORT || 3000,
     router = require('./router')
 
+// socket server
 
+const   server = require('http').createServer(app),
+        io = require('socket.io').listen(server, {cookie: false})
+
+function middlesocket(req, res, next) {
+    res.locals.socketjs = io
+    next()
+}
+
+//
+
+app.use(middlesocket)
 app.set('views', './app/views')
 app.set('view engine', 'ejs')
 app.use(router)
 
-app.listen(port, () => {
-    console.log(`app running on port ${port}`)
+server.listen(process.env.APP_PORT, () => {
+    console.log(`app running on port 4100`);
 })
+
--- app/controllers/api/WelcomeController.js
+++ app/controllers/api/WelcomeController.js
@@ -1,6 +1,16 @@
-const welcome = {
 
+const welcome = {
+    
     index: async (req, res) => {
+
+        const io = res.locals.socketjs
+
+        io.on('connection', (socket) => {
+            console.log('someone connected to socketio')
+            socket.on('welcome', msg => { // menerima dari client
+                io.emit('welcome', msg) // mengirim ke client
+            })
+        })
 
         res.render('pages/index')
 
